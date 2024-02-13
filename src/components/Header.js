818b011a7b3c135000450062f5f5d83768cea636
7vTr0x7 <7vtrox7@gmail.com>
2024-02-13 22:53:42 +0530

    signIn/SignUp/SignOut using firebase api and storing it in redux store

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,43 @@
 import React from "react";
 import logo from "../utils/images/logo.png";
+import userIcon from "../utils/images/user.png";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 ">
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between">
       <img className="w-52 h-24" src={logo} alt="logo" />
+
+      {user && (
+        <div className="flex p-3">
+          <img
+            className="h-12 w-12 rounded-lg m-3"
+            alt="user"
+            src={user.photoURL}
+          />
+          <button
+            className="text-[#e50914] font-bold text-xl"
+            onClick={handleSignOut}>
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,12 +4,19 @@ import { checkValidData, checkValidName } from "../utils/validate";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/slices/useSlice";
 
 const Login = () => {
   const [isSignInFrom, setIsSignInFrom] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const email = useRef(null);
   const password = useRef(null);
@@ -31,6 +38,26 @@ const Login = () => {
       )
         .then((userCredential) => {
           const user = userCredential.user;
+
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: "https://avatars.githubusercontent.com/u/117455929?v=4",
+          })
+            .then(() => {
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              navigate("/browse");
+            })
+            .catch((error) => {
+              setErrorMessage(error.message);
+            });
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -47,6 +74,7 @@ const Login = () => {
         .then((userCredential) => {
           // Signed in
           const user = userCredential.user;
+          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
