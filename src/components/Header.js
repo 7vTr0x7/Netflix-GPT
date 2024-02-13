d37beb77b856e7573f050fccfd6230326bedb811
7vTr0x7 <7vtrox7@gmail.com>
2024-02-13 15:13:17 +0530

    LogIn Form

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import logo from "../utils/logo.png";
 
 const Header = () => {
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black">
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 ">
       <img className="w-52 h-24" src={logo} alt="logo" />
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,11 +5,33 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img
           alt="background-img"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_large.jpg"
         />
+      </div>
+      <div className="flex justify-center ">
+        <form className="bg-black absolute p-12 w-4/12 my-24 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+          <h1 className="font-bold text-3xl pt-4 pb-8 ml-4 ">Sign In</h1>
+          <input
+            type="email"
+            placeholder="Email"
+            className="mb-4 p-3 w-11/12 ml-4 rounded-md bg-[#333]"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="mb-4 p-3 w-11/12 ml-4 rounded-md bg-[#333]"
+          />
+          <button className="p-2 mt-8 mb-4 font-semibold text-lg bg-[#e50914] w-11/12 ml-4 rounded-md">
+            Sign In
+          </button>
+          <div className="flex">
+            <p className="ml-4 mr-2 text-[#737373]">New to Netflix?</p>
+            <p>Sign up Now</p>
+          </div>
+        </form>
       </div>
     </div>
   );
